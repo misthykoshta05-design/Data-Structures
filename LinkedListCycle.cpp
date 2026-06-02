e0aa752b951a28593ecc91c2abcf3063f32f549a
misthykoshta05-design <misthykoshta05@gmail.com>
2026-06-02 16:38:52 +0530

    Implement cycle detection in linked list

---
+++ LinkedListCycle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL){
+            return false;
+        }
+        map<ListNode*,bool>visited;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            if(visited[temp]==true){
+                return true;
+            }
+            visited[temp]=true;
+            temp=temp->next;
+        }
+        return false;
+    }
+};
